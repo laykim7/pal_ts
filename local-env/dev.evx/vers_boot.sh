bb29f68524df446b891313a6fd9c3513d6a52a24
vm142 <vm142@durutronix>
2020-11-30 17:34:55 +0900

    update boot version info

--- local-env/dev.evx/vers_boot.sh
+++ local-env/dev.evx/vers_boot.sh
@@ -3,22 +3,17 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_boot="4e711ee 2020-11-25T18:19:53"
+export ver_boot="b188e83 2020-11-30T17:34:55"
 
 : <<'END'
 ================================================================================================================
-commit log 2020-11-25T18:19:53
+commit log 2020-11-30T17:34:55
 ================================================================================================================
-4e711eec08921fff11f8a71169908011f4b76ea8 Wed, 25 Nov 2020 18:19:51 +0900, tx evcode b enable bug fix
-e9767e9d16ad22ecbba4d6b306f75c3859a4d383 Wed, 25 Nov 2020 18:05:02 +0900, update sw version info
-f92ed02248d917a318d74f8a82dab5a8fc1763cb Wed, 25 Nov 2020 18:05:01 +0900, if evg1 time dont change -> gtp reset 1
-002ffd186324131ea8fff347bbbe40df4ea074f3 Wed, 25 Nov 2020 18:03:29 +0900, update sw version info
-05a17618a7ad0aaa6b25442196e2c267e9b8f293 Wed, 25 Nov 2020 18:03:28 +0900, FMC SFP Link LED bug fix 5
-b5250986ee005beb91bfbd143ae9768716b7fa66 Wed, 25 Nov 2020 18:02:39 +0900, update sw version info
-213af256939eff1717fd654198682ffbd9248a58 Wed, 25 Nov 2020 18:02:38 +0900, FMC SFP Link LED bug fix 4
-2945b7cfd0a231fb29961d03e4bcb56f0f8d901d Wed, 25 Nov 2020 18:00:01 +0900, update sw version info
-1b9cc796a9474eec28d19374a94a81767a640ee6 Wed, 25 Nov 2020 18:00:00 +0900, FMC SFP Link LED bug fix 3
-57da96931c17e173f15ee8d8167ec6ee41150e67 Wed, 25 Nov 2020 17:59:09 +0900, update sw version info
+b188e8320a2958d7cb0551cae8c2506525b7c446 Mon, 30 Nov 2020 17:34:45 +0900, update image version info
+9f11d967be92020e6b86a1053d892c4c4bbaac0e Mon, 30 Nov 2020 17:33:39 +0900, update git push logic
+5db79a4526a1c3a7b6ee1f8e05fb6ff9f7279101 Mon, 30 Nov 2020 17:33:01 +0900, update sm version info
+8b956dc00b100e91864377b1a5f16fd7cb8a5fbb Mon, 30 Nov 2020 17:28:05 +0900, update sm version info
+1ef11e974bd956904d4baf68e23a4381686c2168 Mon, 30 Nov 2020 17:24:01 +0900, refresh
 
 ================================================================================================================
 END
