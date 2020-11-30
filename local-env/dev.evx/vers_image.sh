b188e8320a2958d7cb0551cae8c2506525b7c446
vm142 <vm142@durutronix>
2020-11-30 17:34:45 +0900

    update image version info

--- local-env/dev.evx/vers_image.sh
+++ local-env/dev.evx/vers_image.sh
@@ -3,22 +3,16 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_image="6339cce 2020-11-24T00:05:23"
+export ver_image="9f11d96 2020-11-30T17:34:45"
 
 : <<'END'
 ================================================================================================================
-commit log 2020-11-24T00:05:23
+commit log 2020-11-30T17:34:45
 ================================================================================================================
-6339ccec28e5aec8b167087daf74f031ead33b2b Tue, 24 Nov 2020 00:05:13 +0900, image.ub ntp component miss - fix
-200f3acb9ca4e1035a03b29f83e44690b21cf84f Mon, 23 Nov 2020 16:26:21 +0900, update boot version info
-7f116414c8f0e7bc02430f180222d1f3de59d5b7 Mon, 23 Nov 2020 16:26:13 +0900, boot.bin broadcast cmd function update
-c2095b2818a036594df16e93e60ddd9672f6eaed Mon, 23 Nov 2020 12:17:25 +0900, update sm version info
-152ce4a45e2a3845b4a4a3984b2b7014cdb4fe4d Mon, 23 Nov 2020 12:17:24 +0900, screen auto start update 2
-fe4f8206d6cabb0531b982ace6e735a0bd0cabae Mon, 23 Nov 2020 12:13:53 +0900, update sm version info
-a0d8f5dec71bc4ae66719fc2ca42542d6021f674 Mon, 23 Nov 2020 12:13:52 +0900, screen auto start update 1
-4386c9fe22b96da1f10f767f50073d7868f1d788 Mon, 23 Nov 2020 11:38:37 +0900, update sm version info
-6b423d7e390126d3a95c2459d6a9ac3ee38c9dce Mon, 23 Nov 2020 11:38:36 +0900, screen auto start update
-19be250d0de24a03cc1b8b0514c5f36473574677 Sat, 21 Nov 2020 00:33:06 +0900, update image version info
+9f11d967be92020e6b86a1053d892c4c4bbaac0e Mon, 30 Nov 2020 17:33:39 +0900, update git push logic
+5db79a4526a1c3a7b6ee1f8e05fb6ff9f7279101 Mon, 30 Nov 2020 17:33:01 +0900, update sm version info
+8b956dc00b100e91864377b1a5f16fd7cb8a5fbb Mon, 30 Nov 2020 17:28:05 +0900, update sm version info
+1ef11e974bd956904d4baf68e23a4381686c2168 Mon, 30 Nov 2020 17:24:01 +0900, refresh
 
 ================================================================================================================
 END
