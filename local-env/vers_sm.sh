5887396397d8abe9e410af53538485f967ffbc05
vm142 <vm142@durutronix>
2020-12-03 16:55:31 +0900

    update sm version info

--- local-env/vers_sm.sh
+++ local-env/vers_sm.sh
@@ -3,12 +3,18 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sm="8b956dc 2020-11-30T17:33:01"
+export ver_sm="216b8b8 2020-12-03T16:55:31"
 
 : <<'END'
 ================================================================================================================
-commit log 2020-11-30T17:33:01
+commit log 2020-12-03T16:55:31
 ================================================================================================================
+216b8b8dfbc4e9ab7e9b868ef318223b7bd0dd5a Thu, 3 Dec 2020 16:52:54 +0900, update sw version info
+b9baa5f01aa068a4c64134e4ab0bac7abd2f1f4e Mon, 30 Nov 2020 17:35:28 +0900, update sw version info
+bb29f68524df446b891313a6fd9c3513d6a52a24 Mon, 30 Nov 2020 17:34:55 +0900, update boot version info
+b188e8320a2958d7cb0551cae8c2506525b7c446 Mon, 30 Nov 2020 17:34:45 +0900, update image version info
+9f11d967be92020e6b86a1053d892c4c4bbaac0e Mon, 30 Nov 2020 17:33:39 +0900, update git push logic
+5db79a4526a1c3a7b6ee1f8e05fb6ff9f7279101 Mon, 30 Nov 2020 17:33:01 +0900, update sm version info
 8b956dc00b100e91864377b1a5f16fd7cb8a5fbb Mon, 30 Nov 2020 17:28:05 +0900, update sm version info
 1ef11e974bd956904d4baf68e23a4381686c2168 Mon, 30 Nov 2020 17:24:01 +0900, refresh
 
