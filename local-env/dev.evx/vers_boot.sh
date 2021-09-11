6df34573d2be146cface0e2fedd68919d07b18f2
vm142 <vm142@durutronix>
2021-09-11 16:50:43 +0900

    update boot version info

--- local-env/dev.evx/vers_boot.sh
+++ local-env/dev.evx/vers_boot.sh
@@ -3,17 +3,22 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_boot="b188e83 2020-11-30T17:34:55"
+export ver_boot="eb9e2d3 2021-09-11T16:50:43"
 
 : <<'END'
 ================================================================================================================
-commit log 2020-11-30T17:34:55
+commit log 2021-09-11T16:50:43
 ================================================================================================================
-b188e8320a2958d7cb0551cae8c2506525b7c446 Mon, 30 Nov 2020 17:34:45 +0900, update image version info
-9f11d967be92020e6b86a1053d892c4c4bbaac0e Mon, 30 Nov 2020 17:33:39 +0900, update git push logic
-5db79a4526a1c3a7b6ee1f8e05fb6ff9f7279101 Mon, 30 Nov 2020 17:33:01 +0900, update sm version info
-8b956dc00b100e91864377b1a5f16fd7cb8a5fbb Mon, 30 Nov 2020 17:28:05 +0900, update sm version info
-1ef11e974bd956904d4baf68e23a4381686c2168 Mon, 30 Nov 2020 17:24:01 +0900, refresh
+eb9e2d372bbc336ffb0675e8e3ca42da0bc48374 Sat, 11 Sep 2021 16:50:41 +0900, 60hz sync update
+97d3179fc841670e3271689a55fe2396a978fe63 Sat, 11 Sep 2021 16:43:55 +0900, update sw version info
+05a8d6df4f0b3ad5e1297070ac78e14ea4a84121 Sat, 11 Sep 2021 16:43:54 +0900, 60hz port update
+087902a25e5d7b40094228219da545bbf1a2972e Thu, 3 Dec 2020 17:38:23 +0900, update sm version info
+5ef8a3d9fdaa1d360d129e1fbdf674c83382b528 Thu, 3 Dec 2020 17:38:22 +0900, git update procedure change 1
+94b1e8bf326c217c227ed091f4505ac341e39e94 Thu, 3 Dec 2020 17:29:11 +0900, update sm version info
+5de463d28ebc506ee98293647d45ee4e97b23b3a Thu, 3 Dec 2020 17:29:11 +0900, git update procedure change
+5887396397d8abe9e410af53538485f967ffbc05 Thu, 3 Dec 2020 16:55:31 +0900, update sm version info
+216b8b8dfbc4e9ab7e9b868ef318223b7bd0dd5a Thu, 3 Dec 2020 16:52:54 +0900, update sw version info
+b9baa5f01aa068a4c64134e4ab0bac7abd2f1f4e Mon, 30 Nov 2020 17:35:28 +0900, update sw version info
 
 ================================================================================================================
 END
