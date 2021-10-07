60ab9ac5b92b7c9804d563bd1c62d76b376567e8
vm144 <vm144@durutronix>
2021-10-07 18:50:46 +0900

    update sw version info

--- app/vers_sw.sh
+++ app/vers_sw.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sw="21d8456 2021-10-07T18:49:15"
+export ver_sw="e631936 2021-10-07T18:50:46"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-10-07T18:49:15
+commit log 2021-10-07T18:50:46
 ================================================================================================================
+e631936cb02fb6827f65578f7af4c407b88758c4 Thu, 7 Oct 2021 18:50:45 +0900, refresh
+27fe5be6201f503068dd60369cc79252cf7dc399 Thu, 7 Oct 2021 18:49:15 +0900, update sw version info
 21d8456c0878102863600dd2e88f815898425af2 Thu, 7 Oct 2021 17:23:20 +0900, upload to server
 f7807c1a6ea53a5421e89ee4e3206be97cc6aa5b Thu, 7 Oct 2021 17:22:50 +0900, upload to server
 6df34573d2be146cface0e2fedd68919d07b18f2 Sat, 11 Sep 2021 16:50:43 +0900, update boot version info
@@ -17,8 +19,6 @@ eb9e2d372bbc336ffb0675e8e3ca42da0bc48374 Sat, 11 Sep 2021 16:50:41 +0900, 60hz s
 05a8d6df4f0b3ad5e1297070ac78e14ea4a84121 Sat, 11 Sep 2021 16:43:54 +0900, 60hz port update
 087902a25e5d7b40094228219da545bbf1a2972e Thu, 3 Dec 2020 17:38:23 +0900, update sm version info
 5ef8a3d9fdaa1d360d129e1fbdf674c83382b528 Thu, 3 Dec 2020 17:38:22 +0900, git update procedure change 1
-94b1e8bf326c217c227ed091f4505ac341e39e94 Thu, 3 Dec 2020 17:29:11 +0900, update sm version info
-5de463d28ebc506ee98293647d45ee4e97b23b3a Thu, 3 Dec 2020 17:29:11 +0900, git update procedure change
 
 ================================================================================================================
 END
