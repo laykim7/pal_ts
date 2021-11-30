e8069f41597185ec36e19bad76589cd508fa5c33
vm144 <vm144@durutronix>
2021-11-30 21:10:36 +0900

    update sm version info

--- local-env/vers_sm.sh
+++ local-env/vers_sm.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sm="ec40508 2021-11-30T21:08:10"
+export ver_sm="cd562eb 2021-11-30T21:10:36"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-11-30T21:08:10
+commit log 2021-11-30T21:10:36
 ================================================================================================================
+cd562ebcd786750b5bf8ff7723696f49dc6cbd08 Tue, 30 Nov 2021 21:10:35 +0900, update
+0a2fd518b9e55ac67743e2af7a6f064519ca6e55 Tue, 30 Nov 2021 21:08:10 +0900, update sm version info
 ec405089c9e22f9240618aee329452a124de89bb Tue, 30 Nov 2021 21:08:09 +0900, update
 88f37cc269c6bf7b6a56c81b2fa8571ced85d066 Tue, 30 Nov 2021 21:04:26 +0900, update sm version info
 c4ec87d4b6a6c1526d1bbd894edc95070d6400ff Tue, 30 Nov 2021 21:04:25 +0900, update
@@ -17,8 +19,6 @@ e199e60f9b3e60b7296296534e62328fc08711a2 Tue, 30 Nov 2021 20:57:27 +0900, update
 9b888cc345fa363b49cd76425adc4b6df0f7fc53 Tue, 30 Nov 2021 20:53:52 +0900, update sw version info
 ae9b0edd7304df1b0e2543ef9f598a0d0504aea7 Tue, 30 Nov 2021 20:53:48 +0900, rebuild
 054c4d948fe62b7cb611898051ac2f878dd5501c Tue, 30 Nov 2021 20:43:18 +0900, update sm version info
-b497063859dbf96bf95ce2007ad0c80d42dd7401 Tue, 30 Nov 2021 20:43:18 +0900, r
-0e40d01faa8c8ef6f462f6957e52787b103ce2b1 Tue, 30 Nov 2021 20:32:18 +0900, update sm version info
 
 ================================================================================================================
 END
