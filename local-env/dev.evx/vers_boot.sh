2f88367a0f6ce5163dfe50454e99553c9fbd18a3
vm144 <vm144@durutronix>
2021-11-30 18:59:57 +0900

    update boot version info

--- local-env/dev.evx/vers_boot.sh
+++ local-env/dev.evx/vers_boot.sh
@@ -3,12 +3,13 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_boot="c307537 2021-11-30T18:59:18"
+export ver_boot="37d40f2 2021-11-30T18:59:57"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-11-30T18:59:18
+commit log 2021-11-30T18:59:57
 ================================================================================================================
+37d40f224e60e12aa6bcf12d16fe8628b487af76 Tue, 30 Nov 2021 18:59:18 +0900, update boot version info
 c307537c9d286a83d4894dada1b0a8f706625fa9 Tue, 30 Nov 2021 18:58:06 +0900, revision update
 3110fe91d663653c1703d90109c6ce35f2792420 Tue, 30 Nov 2021 18:56:21 +0900, revision update
 3235bffba0eac9906fa14275ea629c434daf8652 Tue, 30 Nov 2021 18:55:39 +0900, update sw version info
@@ -18,7 +19,6 @@ c307537c9d286a83d4894dada1b0a8f706625fa9 Tue, 30 Nov 2021 18:58:06 +0900, revisi
 653a2a4275e190f0796a5cafe05f6a076b934bc7 Thu, 7 Oct 2021 21:05:32 +0900, envPath change
 60ab9ac5b92b7c9804d563bd1c62d76b376567e8 Thu, 7 Oct 2021 18:50:46 +0900, update sw version info
 e631936cb02fb6827f65578f7af4c407b88758c4 Thu, 7 Oct 2021 18:50:45 +0900, refresh
-27fe5be6201f503068dd60369cc79252cf7dc399 Thu, 7 Oct 2021 18:49:15 +0900, update sw version info
 
 ================================================================================================================
 END
