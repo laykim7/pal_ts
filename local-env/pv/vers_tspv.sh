36e36d719348d6563eff6ea89321b0b43971afc7
vm144 <vm144@durutronix>
2021-12-21 16:07:47 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,22 +3,22 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="5136c8e 2021-12-21T15:46:03"
+export ver_tspv="abff340 2021-12-21T16:07:46"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-12-21T15:46:03
+commit log 2021-12-21T16:07:46
 ================================================================================================================
+abff340eb27f1093554b10207a025a704e0e5c9d Tue, 21 Dec 2021 16:07:39 +0900, evs pv init file change
+0913eef93a915cee9cbddd2910b138b40c9d2d9a Tue, 21 Dec 2021 15:51:18 +0900, update sw version info
+3f41e9a7fc2f5cfa71e958703df4075e68755672 Tue, 21 Dec 2021 15:51:15 +0900, update app
+25997cc6cf920ee427209c117189b513ff3a6166 Tue, 21 Dec 2021 15:46:04 +0900, update tspv version info
 5136c8eaf1a5c4a0665a8279b1dc6b49e8910d43 Tue, 21 Dec 2021 15:46:01 +0900, pv update2
 0f3335f4ffdf5cfaa397f6815366bc83fa434b07 Tue, 21 Dec 2021 15:18:58 +0900, update tspv version info
 d3251835da1c0b9c3de5f58a85b47521ac31f188 Tue, 21 Dec 2021 15:18:45 +0900, pv update
 6f73a88d819aa47c5e9e92ed6493663c2797b69d Mon, 20 Dec 2021 17:16:47 +0900, update boot version info
 72c82ed61bb30ba6f9acb93eb765b16843c20050 Mon, 20 Dec 2021 17:16:44 +0900, evr & dbus 기능 강화
 f1e05a82d598b277e940d44754d80651e8457dd9 Wed, 1 Dec 2021 02:00:17 +0900, update tspv version info
-91c6529713fa7add99103c1401e951c141a6d0bb Wed, 1 Dec 2021 02:00:15 +0900, pv update
-4415235bd6632e2a5139e57e684ec32e931a591c Wed, 1 Dec 2021 01:51:00 +0900, update boot version info
-9412ba21475d65a94e3f86158fde7c82a7fd2b1e Wed, 1 Dec 2021 01:50:58 +0900, ev12~15 bug fix
-562702adc4f3f2df7b7b0a78a3a025a999724132 Wed, 1 Dec 2021 00:50:53 +0900, update boot version info
 
 ================================================================================================================
 END
