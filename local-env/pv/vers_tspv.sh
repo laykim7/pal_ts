d2feb2c3cd1d24718341758fadd40db05fb52f11
vm144 <vm144@durutronix>
2021-12-21 18:04:48 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,22 +3,22 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="2fd9a63 2021-12-21T17:44:07"
+export ver_tspv="955f551 2021-12-21T18:04:48"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-12-21T17:44:07
+commit log 2021-12-21T18:04:48
 ================================================================================================================
+955f551b594f06d5060ffbd9aa56f2fb9b151ff9 Tue, 21 Dec 2021 18:04:47 +0900, evs pv init file change
+acbf899b0f50cc0bc6b13db27394cdadd8d2053d Tue, 21 Dec 2021 18:03:01 +0900, update sw version info
+dd45805ea4fcb3cfd6308ad07ae1eaaa2ca069e9 Tue, 21 Dec 2021 18:02:59 +0900, evg evs -> ch0,1 input update
+0e887aae106349bb404ead275baa4fc4519224cc Tue, 21 Dec 2021 17:44:07 +0900, update tspv version info
 2fd9a6334929a58a1e9994d6a46fa5d81762e348 Tue, 21 Dec 2021 17:44:06 +0900, evs pv init file change
 0a6ca98fdc6443cef66e2949af9294a70f3942f6 Tue, 21 Dec 2021 17:41:38 +0900, update tspv version info
 0f056889515c0237ad67a09e9778f693f1309ac7 Tue, 21 Dec 2021 17:41:37 +0900, evs pv init file change
 ca30d1f78857a1af331789cb333b6c4da85c1489 Tue, 21 Dec 2021 17:37:05 +0900, update tspv version info
 398bab1b1f3d5a28b8a8e57060bcc007a48adafd Tue, 21 Dec 2021 17:37:04 +0900, evs pv init file change
 62182cad6690767e8e17ac68b1f1c3ce09c457ad Tue, 21 Dec 2021 17:06:07 +0900, update tspv version info
-ed8246e7edf9c1ebd6f212db5301b8ccc681a208 Tue, 21 Dec 2021 17:06:06 +0900, evs pv init file change
-01353510f78879d7d67d260952e15da54999926c Tue, 21 Dec 2021 16:54:18 +0900, update tspv version info
-89c7b91b57ffc6497f863c93516b498a2a8aeae7 Tue, 21 Dec 2021 16:54:09 +0900, evs pv init file change
-95100895c0fe217814f0ebd4252ed1aeca8e727f Tue, 21 Dec 2021 16:51:25 +0900, update tspv version info
 
 ================================================================================================================
 END
