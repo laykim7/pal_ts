3664165a74146d66a5cc507114e1d8e49c90911e
vm144 <vm144@durutronix>
2021-12-21 16:38:40 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="32d60d2 2021-12-21T16:32:10"
+export ver_tspv="8173ad6 2021-12-21T16:38:40"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-12-21T16:32:10
+commit log 2021-12-21T16:38:40
 ================================================================================================================
+8173ad639679e07522cdaf2f83400f39a0639a9a Tue, 21 Dec 2021 16:38:39 +0900, evs pv init file change
+60883f76bc8c19edcdec09e963d7249eb6a00387 Tue, 21 Dec 2021 16:32:10 +0900, update tspv version info
 32d60d27ebd0164fe3e71fa4c6114d188cf29775 Tue, 21 Dec 2021 16:32:09 +0900, evs pv init file change
 36e36d719348d6563eff6ea89321b0b43971afc7 Tue, 21 Dec 2021 16:07:47 +0900, update tspv version info
 abff340eb27f1093554b10207a025a704e0e5c9d Tue, 21 Dec 2021 16:07:39 +0900, evs pv init file change
@@ -17,8 +19,6 @@ abff340eb27f1093554b10207a025a704e0e5c9d Tue, 21 Dec 2021 16:07:39 +0900, evs pv
 25997cc6cf920ee427209c117189b513ff3a6166 Tue, 21 Dec 2021 15:46:04 +0900, update tspv version info
 5136c8eaf1a5c4a0665a8279b1dc6b49e8910d43 Tue, 21 Dec 2021 15:46:01 +0900, pv update2
 0f3335f4ffdf5cfaa397f6815366bc83fa434b07 Tue, 21 Dec 2021 15:18:58 +0900, update tspv version info
-d3251835da1c0b9c3de5f58a85b47521ac31f188 Tue, 21 Dec 2021 15:18:45 +0900, pv update
-6f73a88d819aa47c5e9e92ed6493663c2797b69d Mon, 20 Dec 2021 17:16:47 +0900, update boot version info
 
 ================================================================================================================
 END
