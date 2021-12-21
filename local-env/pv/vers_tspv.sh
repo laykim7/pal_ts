25997cc6cf920ee427209c117189b513ff3a6166
vm144 <vm144@durutronix>
2021-12-21 15:46:04 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="d325183 2021-12-21T15:18:57"
+export ver_tspv="5136c8e 2021-12-21T15:46:03"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-12-21T15:18:57
+commit log 2021-12-21T15:46:03
 ================================================================================================================
+5136c8eaf1a5c4a0665a8279b1dc6b49e8910d43 Tue, 21 Dec 2021 15:46:01 +0900, pv update2
+0f3335f4ffdf5cfaa397f6815366bc83fa434b07 Tue, 21 Dec 2021 15:18:58 +0900, update tspv version info
 d3251835da1c0b9c3de5f58a85b47521ac31f188 Tue, 21 Dec 2021 15:18:45 +0900, pv update
 6f73a88d819aa47c5e9e92ed6493663c2797b69d Mon, 20 Dec 2021 17:16:47 +0900, update boot version info
 72c82ed61bb30ba6f9acb93eb765b16843c20050 Mon, 20 Dec 2021 17:16:44 +0900, evr & dbus 기능 강화
@@ -17,8 +19,6 @@ f1e05a82d598b277e940d44754d80651e8457dd9 Wed, 1 Dec 2021 02:00:17 +0900, update 
 4415235bd6632e2a5139e57e684ec32e931a591c Wed, 1 Dec 2021 01:51:00 +0900, update boot version info
 9412ba21475d65a94e3f86158fde7c82a7fd2b1e Wed, 1 Dec 2021 01:50:58 +0900, ev12~15 bug fix
 562702adc4f3f2df7b7b0a78a3a025a999724132 Wed, 1 Dec 2021 00:50:53 +0900, update boot version info
-48a642700159b9b83c73e74da60b5d82e1378ccc Wed, 1 Dec 2021 00:50:50 +0900, sync ev12~15
-a0409537e83e912d9365fc420d8ba2896c779a82 Wed, 1 Dec 2021 00:32:40 +0900, update sw version info
 
 ================================================================================================================
 END
