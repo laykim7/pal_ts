a1ffe6dfe10f95c86d94678e14ff2a502a1c65da
vm144 <vm144@durutronix>
2022-01-13 19:58:07 +0900

    update sw version info

--- app/vers_sw.sh
+++ app/vers_sw.sh
@@ -3,22 +3,22 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sw="dd45805 2021-12-21T18:03:01"
+export ver_sw="9a6c397 2022-01-13T19:58:07"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-12-21T18:03:01
+commit log 2022-01-13T19:58:07
 ================================================================================================================
+9a6c397d0769dfc6152a3ee066007ad48067b054 Thu, 13 Jan 2022 19:58:03 +0900, remove evr mapping ram waveform
+8a2b81e6ecdf218bc44fc0364527f4c3f90d5af2 Mon, 27 Dec 2021 15:23:52 +0900, update tspv version info
+04beef7f1f292292f4d2d5b73e7d91d990ff7ee8 Mon, 27 Dec 2021 15:23:44 +0900, evf cps pv init value change
+d2feb2c3cd1d24718341758fadd40db05fb52f11 Tue, 21 Dec 2021 18:04:48 +0900, update tspv version info
+955f551b594f06d5060ffbd9aa56f2fb9b151ff9 Tue, 21 Dec 2021 18:04:47 +0900, evs pv init file change
+acbf899b0f50cc0bc6b13db27394cdadd8d2053d Tue, 21 Dec 2021 18:03:01 +0900, update sw version info
 dd45805ea4fcb3cfd6308ad07ae1eaaa2ca069e9 Tue, 21 Dec 2021 18:02:59 +0900, evg evs -> ch0,1 input update
 0e887aae106349bb404ead275baa4fc4519224cc Tue, 21 Dec 2021 17:44:07 +0900, update tspv version info
 2fd9a6334929a58a1e9994d6a46fa5d81762e348 Tue, 21 Dec 2021 17:44:06 +0900, evs pv init file change
 0a6ca98fdc6443cef66e2949af9294a70f3942f6 Tue, 21 Dec 2021 17:41:38 +0900, update tspv version info
-0f056889515c0237ad67a09e9778f693f1309ac7 Tue, 21 Dec 2021 17:41:37 +0900, evs pv init file change
-ca30d1f78857a1af331789cb333b6c4da85c1489 Tue, 21 Dec 2021 17:37:05 +0900, update tspv version info
-398bab1b1f3d5a28b8a8e57060bcc007a48adafd Tue, 21 Dec 2021 17:37:04 +0900, evs pv init file change
-62182cad6690767e8e17ac68b1f1c3ce09c457ad Tue, 21 Dec 2021 17:06:07 +0900, update tspv version info
-ed8246e7edf9c1ebd6f212db5301b8ccc681a208 Tue, 21 Dec 2021 17:06:06 +0900, evs pv init file change
-01353510f78879d7d67d260952e15da54999926c Tue, 21 Dec 2021 16:54:18 +0900, update tspv version info
 
 ================================================================================================================
 END
