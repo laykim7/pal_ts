cc2aa1382618ea4987c80e77027ad6136d7b84b8
vm144 <vm144@durutronix>
2022-01-13 20:48:35 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,22 +3,22 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="04beef7 2021-12-27T15:23:52"
+export ver_tspv="f30ada5 2022-01-13T20:48:35"
 
 : <<'END'
 ================================================================================================================
-commit log 2021-12-27T15:23:52
+commit log 2022-01-13T20:48:35
 ================================================================================================================
+f30ada5e4b1206d2bcd9feff249923da7f666a2d Thu, 13 Jan 2022 20:48:33 +0900, pv list & init files update
+a1ffe6dfe10f95c86d94678e14ff2a502a1c65da Thu, 13 Jan 2022 19:58:07 +0900, update sw version info
+9a6c397d0769dfc6152a3ee066007ad48067b054 Thu, 13 Jan 2022 19:58:03 +0900, remove evr mapping ram waveform
+8a2b81e6ecdf218bc44fc0364527f4c3f90d5af2 Mon, 27 Dec 2021 15:23:52 +0900, update tspv version info
 04beef7f1f292292f4d2d5b73e7d91d990ff7ee8 Mon, 27 Dec 2021 15:23:44 +0900, evf cps pv init value change
 d2feb2c3cd1d24718341758fadd40db05fb52f11 Tue, 21 Dec 2021 18:04:48 +0900, update tspv version info
 955f551b594f06d5060ffbd9aa56f2fb9b151ff9 Tue, 21 Dec 2021 18:04:47 +0900, evs pv init file change
 acbf899b0f50cc0bc6b13db27394cdadd8d2053d Tue, 21 Dec 2021 18:03:01 +0900, update sw version info
 dd45805ea4fcb3cfd6308ad07ae1eaaa2ca069e9 Tue, 21 Dec 2021 18:02:59 +0900, evg evs -> ch0,1 input update
 0e887aae106349bb404ead275baa4fc4519224cc Tue, 21 Dec 2021 17:44:07 +0900, update tspv version info
-2fd9a6334929a58a1e9994d6a46fa5d81762e348 Tue, 21 Dec 2021 17:44:06 +0900, evs pv init file change
-0a6ca98fdc6443cef66e2949af9294a70f3942f6 Tue, 21 Dec 2021 17:41:38 +0900, update tspv version info
-0f056889515c0237ad67a09e9778f693f1309ac7 Tue, 21 Dec 2021 17:41:37 +0900, evs pv init file change
-ca30d1f78857a1af331789cb333b6c4da85c1489 Tue, 21 Dec 2021 17:37:05 +0900, update tspv version info
 
 ================================================================================================================
 END
