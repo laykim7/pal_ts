9855e7d233efc8ea8e74f8fcac2c2311ece97e30
vm144 <vm144@durutronix>
2022-01-14 14:08:01 +0900

    sdcard top files upload

---
+++ sdCard/verl_boot.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Version determined from Git #
+# Generated file, do not edit! #
+
+export ver_boot_old="72c82ed 2021-12-20T17:16:47"
+
+: <<'END'
+================================================================================================================
+commit log 2021-12-20T17:16:47
+================================================================================================================
+72c82ed61bb30ba6f9acb93eb765b16843c20050 Mon, 20 Dec 2021 17:16:44 +0900, evr & dbus 기능 강화
+f1e05a82d598b277e940d44754d80651e8457dd9 Wed, 1 Dec 2021 02:00:17 +0900, update tspv version info
+91c6529713fa7add99103c1401e951c141a6d0bb Wed, 1 Dec 2021 02:00:15 +0900, pv update
+4415235bd6632e2a5139e57e684ec32e931a591c Wed, 1 Dec 2021 01:51:00 +0900, update boot version info
+9412ba21475d65a94e3f86158fde7c82a7fd2b1e Wed, 1 Dec 2021 01:50:58 +0900, ev12~15 bug fix
+562702adc4f3f2df7b7b0a78a3a025a999724132 Wed, 1 Dec 2021 00:50:53 +0900, update boot version info
+48a642700159b9b83c73e74da60b5d82e1378ccc Wed, 1 Dec 2021 00:50:50 +0900, sync ev12~15
+a0409537e83e912d9365fc420d8ba2896c779a82 Wed, 1 Dec 2021 00:32:40 +0900, update sw version info
+f36d112059848c32a47b41121e04f44ce2136b9e Wed, 1 Dec 2021 00:32:38 +0900, sync, ev12~15 add
+859fabbe110555b8b35f3f5bfe07ddacd6e054ab Tue, 30 Nov 2021 21:27:56 +0900, update sm version info
+
+================================================================================================================
+END
+
---
+++ sdCard/verl_sm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Version determined from Git #
+# Generated file, do not edit! #
+
+export ver_sm_old="b593353 2021-11-30T21:27:56"
+
+: <<'END'
+================================================================================================================
+commit log 2021-11-30T21:27:56
+================================================================================================================
+b593353890f4c0521501da3fe82ff7e387a80df9 Tue, 30 Nov 2021 21:27:55 +0900, update
+79cd41573174796cfc5484f2c6c7b5eee0b1ab47 Tue, 30 Nov 2021 21:18:28 +0900, update sm version info
+ea79e97fb4453d82752f98a7112782abb689762a Tue, 30 Nov 2021 21:18:27 +0900, update
+e8069f41597185ec36e19bad76589cd508fa5c33 Tue, 30 Nov 2021 21:10:36 +0900, update sm version info
+cd562ebcd786750b5bf8ff7723696f49dc6cbd08 Tue, 30 Nov 2021 21:10:35 +0900, update
+0a2fd518b9e55ac67743e2af7a6f064519ca6e55 Tue, 30 Nov 2021 21:08:10 +0900, update sm version info
+ec405089c9e22f9240618aee329452a124de89bb Tue, 30 Nov 2021 21:08:09 +0900, update
+88f37cc269c6bf7b6a56c81b2fa8571ced85d066 Tue, 30 Nov 2021 21:04:26 +0900, update sm version info
+c4ec87d4b6a6c1526d1bbd894edc95070d6400ff Tue, 30 Nov 2021 21:04:25 +0900, update
+e199e60f9b3e60b7296296534e62328fc08711a2 Tue, 30 Nov 2021 20:57:27 +0900, update sm version info
+
+================================================================================================================
+END
+
