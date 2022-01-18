579be5689e3fbb98493d0175accde8116ca28d04
pal <pal@example.com>
2022-01-18 17:26:42 +0900

    update sm version info

--- local-env/vers_sm.sh
+++ local-env/vers_sm.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sm="6b6eaa7 2022-01-18T17:12:43"
+export ver_sm="52407ae 2022-01-18T17:26:42"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-01-18T17:12:43
+commit log 2022-01-18T17:26:42
 ================================================================================================================
+52407ae05bdee8fde72325b9a27d469626f689cd Tue, 18 Jan 2022 17:26:41 +0900, expect update
+84e2c17571e926e0649ab7aabaca4fa9bef6edf9 Tue, 18 Jan 2022 17:12:43 +0900, update sm version info
 6b6eaa710c0fee0b6338a183deb2444a96481eb5 Tue, 18 Jan 2022 17:12:29 +0900, test
 a4c03214126725ca75f59e4b63feac66dea3b9c7 Tue, 18 Jan 2022 17:08:07 +0900, update sm version info
 d968ad1ae9b94695c204bee9f7c917f96a887a05 Tue, 18 Jan 2022 17:07:23 +0900, update sm version info
@@ -17,8 +19,6 @@ e2254bf4a0db305d3b9d0060344a346eeb04dac8 Tue, 18 Jan 2022 17:02:01 +0900, update
 298d43a6f16700fb51bc61bda3293508fddc8c6d Tue, 18 Jan 2022 11:37:16 +0900, remove auto start
 9855e7d233efc8ea8e74f8fcac2c2311ece97e30 Fri, 14 Jan 2022 14:08:01 +0900, sdcard top files upload
 8db84490e948a69de896a5c1e5725a31cb036d31 Thu, 13 Jan 2022 20:51:35 +0900, ..
-e1ae45be6c02c34fbf1eff0fd62431373ad67095 Thu, 13 Jan 2022 20:49:14 +0900, update tspv version info
-6d13520d4ef98085ec8b171bb75fdc606680dc1d Thu, 13 Jan 2022 20:48:51 +0900, update tspv version info
 
 ================================================================================================================
 END
