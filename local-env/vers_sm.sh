d968ad1ae9b94695c204bee9f7c917f96a887a05
pal <pal@example.com>
2022-01-18 17:07:23 +0900

    update sm version info

--- local-env/vers_sm.sh
+++ local-env/vers_sm.sh
@@ -3,12 +3,13 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sm="938b22c 2022-01-18T17:02:01"
+export ver_sm="e2254bf 2022-01-18T17:07:23"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-01-18T17:02:01
+commit log 2022-01-18T17:07:23
 ================================================================================================================
+e2254bf4a0db305d3b9d0060344a346eeb04dac8 Tue, 18 Jan 2022 17:02:01 +0900, update sm version info
 938b22caf0a365ba14a7e5ad76d8843098ec699d Tue, 18 Jan 2022 17:00:38 +0900, test
 298d43a6f16700fb51bc61bda3293508fddc8c6d Tue, 18 Jan 2022 11:37:16 +0900, remove auto start
 9855e7d233efc8ea8e74f8fcac2c2311ece97e30 Fri, 14 Jan 2022 14:08:01 +0900, sdcard top files upload
@@ -18,7 +19,6 @@ e1ae45be6c02c34fbf1eff0fd62431373ad67095 Thu, 13 Jan 2022 20:49:14 +0900, update
 cc2aa1382618ea4987c80e77027ad6136d7b84b8 Thu, 13 Jan 2022 20:48:35 +0900, update tspv version info
 f30ada5e4b1206d2bcd9feff249923da7f666a2d Thu, 13 Jan 2022 20:48:33 +0900, pv list & init files update
 a1ffe6dfe10f95c86d94678e14ff2a502a1c65da Thu, 13 Jan 2022 19:58:07 +0900, update sw version info
-9a6c397d0769dfc6152a3ee066007ad48067b054 Thu, 13 Jan 2022 19:58:03 +0900, remove evr mapping ram waveform
 
 ================================================================================================================
 END
