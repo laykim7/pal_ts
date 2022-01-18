a4c03214126725ca75f59e4b63feac66dea3b9c7
pal <pal@example.com>
2022-01-18 17:08:07 +0900

    update sm version info

--- local-env/vers_sm.sh
+++ local-env/vers_sm.sh
@@ -3,12 +3,13 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sm="e2254bf 2022-01-18T17:07:23"
+export ver_sm="d968ad1 2022-01-18T17:08:07"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-01-18T17:07:23
+commit log 2022-01-18T17:08:07
 ================================================================================================================
+d968ad1ae9b94695c204bee9f7c917f96a887a05 Tue, 18 Jan 2022 17:07:23 +0900, update sm version info
 e2254bf4a0db305d3b9d0060344a346eeb04dac8 Tue, 18 Jan 2022 17:02:01 +0900, update sm version info
 938b22caf0a365ba14a7e5ad76d8843098ec699d Tue, 18 Jan 2022 17:00:38 +0900, test
 298d43a6f16700fb51bc61bda3293508fddc8c6d Tue, 18 Jan 2022 11:37:16 +0900, remove auto start
@@ -18,7 +19,6 @@ e1ae45be6c02c34fbf1eff0fd62431373ad67095 Thu, 13 Jan 2022 20:49:14 +0900, update
 6d13520d4ef98085ec8b171bb75fdc606680dc1d Thu, 13 Jan 2022 20:48:51 +0900, update tspv version info
 cc2aa1382618ea4987c80e77027ad6136d7b84b8 Thu, 13 Jan 2022 20:48:35 +0900, update tspv version info
 f30ada5e4b1206d2bcd9feff249923da7f666a2d Thu, 13 Jan 2022 20:48:33 +0900, pv list & init files update
-a1ffe6dfe10f95c86d94678e14ff2a502a1c65da Thu, 13 Jan 2022 19:58:07 +0900, update sw version info
 
 ================================================================================================================
 END
