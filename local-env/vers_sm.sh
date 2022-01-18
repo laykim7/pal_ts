84e2c17571e926e0649ab7aabaca4fa9bef6edf9
pal <pal@example.com>
2022-01-18 17:12:43 +0900

    update sm version info

--- local-env/vers_sm.sh
+++ local-env/vers_sm.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sm="d968ad1 2022-01-18T17:08:07"
+export ver_sm="6b6eaa7 2022-01-18T17:12:43"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-01-18T17:08:07
+commit log 2022-01-18T17:12:43
 ================================================================================================================
+6b6eaa710c0fee0b6338a183deb2444a96481eb5 Tue, 18 Jan 2022 17:12:29 +0900, test
+a4c03214126725ca75f59e4b63feac66dea3b9c7 Tue, 18 Jan 2022 17:08:07 +0900, update sm version info
 d968ad1ae9b94695c204bee9f7c917f96a887a05 Tue, 18 Jan 2022 17:07:23 +0900, update sm version info
 e2254bf4a0db305d3b9d0060344a346eeb04dac8 Tue, 18 Jan 2022 17:02:01 +0900, update sm version info
 938b22caf0a365ba14a7e5ad76d8843098ec699d Tue, 18 Jan 2022 17:00:38 +0900, test
@@ -17,8 +19,6 @@ e2254bf4a0db305d3b9d0060344a346eeb04dac8 Tue, 18 Jan 2022 17:02:01 +0900, update
 8db84490e948a69de896a5c1e5725a31cb036d31 Thu, 13 Jan 2022 20:51:35 +0900, ..
 e1ae45be6c02c34fbf1eff0fd62431373ad67095 Thu, 13 Jan 2022 20:49:14 +0900, update tspv version info
 6d13520d4ef98085ec8b171bb75fdc606680dc1d Thu, 13 Jan 2022 20:48:51 +0900, update tspv version info
-cc2aa1382618ea4987c80e77027ad6136d7b84b8 Thu, 13 Jan 2022 20:48:35 +0900, update tspv version info
-f30ada5e4b1206d2bcd9feff249923da7f666a2d Thu, 13 Jan 2022 20:48:33 +0900, pv list & init files update
 
 ================================================================================================================
 END
