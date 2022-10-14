4c847792b724835c8b7c2e5b7f3dda36c42646a7
laykim <laykim@dandansys.com>
2022-10-14 21:47:23 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="a0ecbf1 2022-10-14T21:33:11"
+export ver_tspv="82b7645 2022-10-14T21:47:23"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-10-14T21:33:11
+commit log 2022-10-14T21:47:23
 ================================================================================================================
+82b76454276875a4258416468b0e76873aef9029 Fri, 14 Oct 2022 21:47:22 +0900, upload pv init files evs2, evr4~7 update for test
+58b3b63233c63e668bc912a538e30b6f5dc2fdd5 Fri, 14 Oct 2022 21:33:11 +0900, update tspv version info
 a0ecbf11ac62df1c1a7e75a4148b4cda43dd2c96 Fri, 14 Oct 2022 21:33:09 +0900, upload pv init files evs2, evr4~7
 4f86fa39675f6445529c45b221f5d260ece40b4a Fri, 14 Oct 2022 17:54:26 +0900, update sw version info
 c4c2ac7045b21606b79ab4dbc31abb80942eec80 Fri, 14 Oct 2022 17:51:25 +0900, add dev.evx0
@@ -17,8 +19,6 @@ c4c2ac7045b21606b79ab4dbc31abb80942eec80 Fri, 14 Oct 2022 17:51:25 +0900, add de
 8db84490e948a69de896a5c1e5725a31cb036d31 Thu, 13 Jan 2022 20:51:35 +0900, ..
 e1ae45be6c02c34fbf1eff0fd62431373ad67095 Thu, 13 Jan 2022 20:49:14 +0900, update tspv version info
 6d13520d4ef98085ec8b171bb75fdc606680dc1d Thu, 13 Jan 2022 20:48:51 +0900, update tspv version info
-cc2aa1382618ea4987c80e77027ad6136d7b84b8 Thu, 13 Jan 2022 20:48:35 +0900, update tspv version info
-f30ada5e4b1206d2bcd9feff249923da7f666a2d Thu, 13 Jan 2022 20:48:33 +0900, pv list & init files update
 
 ================================================================================================================
 END
