aae095c0047004dc07aba18b9b6feedb13bd5da0
laykim <laykim@dandansys.com>
2022-10-17 10:58:58 +0900

    update sw version info

--- app/vers_sw.sh
+++ app/vers_sw.sh
@@ -3,22 +3,22 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sw="c4c2ac7 2022-10-14T17:54:26"
+export ver_sw="dd49a62 2022-10-17T10:58:58"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-10-14T17:54:26
+commit log 2022-10-17T10:58:58
 ================================================================================================================
+dd49a629cd528b07d0a73422c0ab070a8a118b78 Mon, 17 Oct 2022 10:58:54 +0900, evr map rma init max value bug fix 1
+4c847792b724835c8b7c2e5b7f3dda36c42646a7 Fri, 14 Oct 2022 21:47:23 +0900, update tspv version info
+82b76454276875a4258416468b0e76873aef9029 Fri, 14 Oct 2022 21:47:22 +0900, upload pv init files evs2, evr4~7 update for test
+58b3b63233c63e668bc912a538e30b6f5dc2fdd5 Fri, 14 Oct 2022 21:33:11 +0900, update tspv version info
+a0ecbf11ac62df1c1a7e75a4148b4cda43dd2c96 Fri, 14 Oct 2022 21:33:09 +0900, upload pv init files evs2, evr4~7
+4f86fa39675f6445529c45b221f5d260ece40b4a Fri, 14 Oct 2022 17:54:26 +0900, update sw version info
 c4c2ac7045b21606b79ab4dbc31abb80942eec80 Fri, 14 Oct 2022 17:51:25 +0900, add dev.evx0
 298d43a6f16700fb51bc61bda3293508fddc8c6d Tue, 18 Jan 2022 11:37:16 +0900, remove auto start
 9855e7d233efc8ea8e74f8fcac2c2311ece97e30 Fri, 14 Jan 2022 14:08:01 +0900, sdcard top files upload
 8db84490e948a69de896a5c1e5725a31cb036d31 Thu, 13 Jan 2022 20:51:35 +0900, ..
-e1ae45be6c02c34fbf1eff0fd62431373ad67095 Thu, 13 Jan 2022 20:49:14 +0900, update tspv version info
-6d13520d4ef98085ec8b171bb75fdc606680dc1d Thu, 13 Jan 2022 20:48:51 +0900, update tspv version info
-cc2aa1382618ea4987c80e77027ad6136d7b84b8 Thu, 13 Jan 2022 20:48:35 +0900, update tspv version info
-f30ada5e4b1206d2bcd9feff249923da7f666a2d Thu, 13 Jan 2022 20:48:33 +0900, pv list & init files update
-a1ffe6dfe10f95c86d94678e14ff2a502a1c65da Thu, 13 Jan 2022 19:58:07 +0900, update sw version info
-9a6c397d0769dfc6152a3ee066007ad48067b054 Thu, 13 Jan 2022 19:58:03 +0900, remove evr mapping ram waveform
 
 ================================================================================================================
 END
