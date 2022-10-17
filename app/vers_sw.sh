710475a914149fb648b05bad78c8236898135300
laykim <laykim@dandansys.com>
2022-10-17 11:21:30 +0900

    update sw version info

--- app/vers_sw.sh
+++ app/vers_sw.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sw="56cccbe 2022-10-17T11:15:16"
+export ver_sw="9c7bb2a 2022-10-17T11:21:30"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-10-17T11:15:16
+commit log 2022-10-17T11:21:30
 ================================================================================================================
+9c7bb2a4f0c47dcd88109410569ead8a8a5d9fc7 Mon, 17 Oct 2022 11:21:27 +0900, evr map rma init max value bug fix 4
+f410c6864f925565195fefff476c26f9b658011b Mon, 17 Oct 2022 11:15:16 +0900, update sw version info
 56cccbe98e071eeb2c2d577680e10fe81a25d4d7 Mon, 17 Oct 2022 11:15:13 +0900, evr map rma init max value bug fix 3
 bd59dd356cc1238f6fe526e6068c883f1cadd816 Mon, 17 Oct 2022 11:13:45 +0900, update sw version info
 aaa19866f2ea059fd3190eaa1c76e08cfd950bd8 Mon, 17 Oct 2022 11:13:44 +0900, evr map rma init max value bug fix 3
@@ -17,8 +19,6 @@ a2cce02eff0b9de44f21052132bcddf3e99082ff Mon, 17 Oct 2022 11:09:21 +0900, update
 aae095c0047004dc07aba18b9b6feedb13bd5da0 Mon, 17 Oct 2022 10:58:58 +0900, update sw version info
 dd49a629cd528b07d0a73422c0ab070a8a118b78 Mon, 17 Oct 2022 10:58:54 +0900, evr map rma init max value bug fix 1
 4c847792b724835c8b7c2e5b7f3dda36c42646a7 Fri, 14 Oct 2022 21:47:23 +0900, update tspv version info
-82b76454276875a4258416468b0e76873aef9029 Fri, 14 Oct 2022 21:47:22 +0900, upload pv init files evs2, evr4~7 update for test
-58b3b63233c63e668bc912a538e30b6f5dc2fdd5 Fri, 14 Oct 2022 21:33:11 +0900, update tspv version info
 
 ================================================================================================================
 END
