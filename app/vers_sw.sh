a2cce02eff0b9de44f21052132bcddf3e99082ff
laykim <laykim@dandansys.com>
2022-10-17 11:09:21 +0900

    update sw version info

--- app/vers_sw.sh
+++ app/vers_sw.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_sw="dd49a62 2022-10-17T10:58:58"
+export ver_sw="2d5e6ed 2022-10-17T11:09:21"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-10-17T10:58:58
+commit log 2022-10-17T11:09:21
 ================================================================================================================
+2d5e6ed27c07e64adb6f7d9234cbb9a006a987c3 Mon, 17 Oct 2022 11:09:19 +0900, evr map rma init max value bug fix 2
+aae095c0047004dc07aba18b9b6feedb13bd5da0 Mon, 17 Oct 2022 10:58:58 +0900, update sw version info
 dd49a629cd528b07d0a73422c0ab070a8a118b78 Mon, 17 Oct 2022 10:58:54 +0900, evr map rma init max value bug fix 1
 4c847792b724835c8b7c2e5b7f3dda36c42646a7 Fri, 14 Oct 2022 21:47:23 +0900, update tspv version info
 82b76454276875a4258416468b0e76873aef9029 Fri, 14 Oct 2022 21:47:22 +0900, upload pv init files evs2, evr4~7 update for test
@@ -17,8 +19,6 @@ a0ecbf11ac62df1c1a7e75a4148b4cda43dd2c96 Fri, 14 Oct 2022 21:33:09 +0900, upload
 4f86fa39675f6445529c45b221f5d260ece40b4a Fri, 14 Oct 2022 17:54:26 +0900, update sw version info
 c4c2ac7045b21606b79ab4dbc31abb80942eec80 Fri, 14 Oct 2022 17:51:25 +0900, add dev.evx0
 298d43a6f16700fb51bc61bda3293508fddc8c6d Tue, 18 Jan 2022 11:37:16 +0900, remove auto start
-9855e7d233efc8ea8e74f8fcac2c2311ece97e30 Fri, 14 Jan 2022 14:08:01 +0900, sdcard top files upload
-8db84490e948a69de896a5c1e5725a31cb036d31 Thu, 13 Jan 2022 20:51:35 +0900, ..
 
 ================================================================================================================
 END
