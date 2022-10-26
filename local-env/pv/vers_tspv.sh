b217767ed0f13060816b59c2518af0b6bb7abf72
laykim <laykim@dandansys.com>
2022-10-26 16:33:57 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="b4493bf 2022-10-26T16:23:28"
+export ver_tspv="d03c55d 2022-10-26T16:33:57"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-10-26T16:23:28
+commit log 2022-10-26T16:33:57
 ================================================================================================================
+d03c55d9b00f47150f3b2a279e319d4c7047a4d6 Wed, 26 Oct 2022 16:33:55 +0900, update
+4d86f0e09c36e6f9b2a206614f8fd6bebef4e5bf Wed, 26 Oct 2022 16:23:28 +0900, update tspv version info
 b4493bf6b2a964b5efb1c11f28ae9b1d11b55f8b Wed, 26 Oct 2022 16:17:55 +0900, io port freq out change 120~0.5
 a42629d74c7a383a7ec34e0253d5f6afe905ce2f Fri, 21 Oct 2022 16:45:09 +0900, pv init update - evcode various freq 120,60,40,30,20,15,12,~
 a3741e1d1433379da1196e21e7326da8bdc2e2c2 Mon, 17 Oct 2022 12:01:00 +0900, pv init update
@@ -17,8 +19,6 @@ a3741e1d1433379da1196e21e7326da8bdc2e2c2 Mon, 17 Oct 2022 12:01:00 +0900, pv ini
 4f27f7fe1baeb9aaae0476ee2822a5d5584eca2d Mon, 17 Oct 2022 11:46:40 +0900, update sw version info
 e439db1ca717abad830122034b0d8ae7a302bf45 Mon, 17 Oct 2022 11:46:38 +0900, evr map rma init max value bug fix 7
 c34e0e64cc32067aad460aebf4a0156571423f54 Mon, 17 Oct 2022 11:29:43 +0900, update sw version info
-d30a3299dc4f512a66c820f7611c0d55ba3e707c Mon, 17 Oct 2022 11:29:42 +0900, evr map rma init max value bug fix 6
-09fb42f3c186980bb593ad6db50f3daa7eb228e0 Mon, 17 Oct 2022 11:29:22 +0900, update sw version info
 
 ================================================================================================================
 END
