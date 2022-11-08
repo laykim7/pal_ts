4a0e8bfa2531c913617973a213a015ac075d2ffb
pal <pal>
2022-11-08 16:32:02 +0900

    update tspv version info

--- local-env/pv/vers_tspv.sh
+++ local-env/pv/vers_tspv.sh
@@ -3,12 +3,14 @@
 # Version determined from Git #
 # Generated file, do not edit! #
 
-export ver_tspv="d51c0eb 2022-11-08T16:14:06"
+export ver_tspv="d8cb8bf 2022-11-08T16:32:02"
 
 : <<'END'
 ================================================================================================================
-commit log 2022-11-08T16:14:06
+commit log 2022-11-08T16:32:02
 ================================================================================================================
+d8cb8bf7458e77f63329d58fc5c98ebe9e8cf8a5 Tue, 8 Nov 2022 16:31:58 +0900, update pv ver info
+28667e6422828564705282612e86baf3fbe533ca Tue, 8 Nov 2022 16:14:07 +0900, update tspv version info
 d51c0ebbbeeab10baeb7aa0e1fe906c2937cc0bd Tue, 8 Nov 2022 16:12:05 +0900, pv val change g, r1,2,3,4
 fc87165ae9352aac80e747b18d2d27f893d8d7d3 Thu, 27 Oct 2022 18:04:58 +0900, pv up
 1b658137f4a24ce90c640f7c2a20124f0b0cbe70 Thu, 27 Oct 2022 17:53:11 +0900, pv up
@@ -17,8 +19,6 @@ fc87165ae9352aac80e747b18d2d27f893d8d7d3 Thu, 27 Oct 2022 18:04:58 +0900, pv up
 d8fe93a15f3b7ab81afc202bcf05235b2ced2e7c Thu, 27 Oct 2022 16:45:47 +0900, pv init value update
 286e8dcf22fec22bbf86b6f6e3b512e890074c2b Thu, 27 Oct 2022 15:24:25 +0900, ntp.cfg change 199
 3db37b09db0cf609164135b5cba2a72c5e8b33ec Thu, 27 Oct 2022 14:52:27 +0900, sw 32bit bug fix and 120hz evr port out pv seq update
-e15f8041cc1a318489b25600559fdfaca69834e1 Thu, 20 Jan 2022 21:30:40 +0900, pv init value modify for sync (mxc change number)
-f279d5827c7e0493a21edf25c89bc57e83b51a45 Thu, 20 Jan 2022 21:28:51 +0900, pv init value modify for sync (mxc change number)
 
 ================================================================================================================
 END
